60e58106b7af28392b10e82a8720da9f43b7757d
Heiti Tobi <heiti.tobi@nortal.com>
2024-08-21 11:31:50 +0300

    SIGA-741 Improve hashcode step usage

--- src/main/groovy/ee/openeid/siga/test/GenericSpecification.groovy
+++ src/main/groovy/ee/openeid/siga/test/GenericSpecification.groovy
@@ -3,14 +3,16 @@ package ee.openeid.siga.test
 import ee.openeid.siga.test.request.DatafileRequests
 import ee.openeid.siga.test.request.HashcodeRequests
 import ee.openeid.siga.test.step.DatafileSteps
+import ee.openeid.siga.test.step.HashcodeSteps
 import spock.lang.Shared
 import spock.lang.Specification
 
 abstract class GenericSpecification extends Specification {
     static BeforeAll beforeAll = BeforeAll.instance
     static DatafileRequests datafileRequests = DatafileRequests.instance
-    static HashcodeRequests hashcode = HashcodeRequests.instance
     static DatafileSteps datafile = DatafileSteps.instance
+    static HashcodeRequests hashcodeRequests = HashcodeRequests.instance
+    static HashcodeSteps hashcode = HashcodeSteps.instance
 
     @Shared
     TestConfig conf = ConfigHolder.getConf()
