4ab28e2f2d25d207fc9c947a5fbdbf1b7ce4b076
Heiti Tobi <Heiti.Tobi@nortal.com>
2026-06-29 18:56:05 +0300

    SIGA-1330 Update timestamped ASiC-S validation report tests due SIVA-914

--- src/test/groovy/ee/openeid/siga/test/datafile/validationReport/ValidationSpec.groovy
+++ src/test/groovy/ee/openeid/siga/test/datafile/validationReport/ValidationSpec.groovy
@@ -52,7 +52,7 @@ class ValidationSpec extends GenericSpecification {
                 .body("timeStampTokens[0].warning", hasSize(0))
 //                .body("timeStampTokens[0].errors", hasSize(1))
 //                .body("timeStampTokens[0].errors[0].content[0]", is("The time-stamp message imprint is not intact!"))
-                .body("timeStampTokens[0].certificates", hasSize(3))
+                .body("timeStampTokens[0].certificates", hasSize(1))
                 .body("timeStampTokens[0].certificates[0].commonName", is("DEMO SK TIMESTAMPING AUTHORITY 2023E"))
                 .body("timeStampTokens[0].certificates[0].type", is("CONTENT_TIMESTAMP"))
                 .body("timeStampTokens[0].certificates[0].content", startsWith("MIIDEjCCApigAwIBAgIQM7BQCImkdt18qWDYdbfOtjAKBggqhkjOP"))
@@ -62,7 +62,7 @@ class ValidationSpec extends GenericSpecification {
                 .body("timeStampTokens[1].timestampLevel", is("QTSA"))
                 .body("timeStampTokens[1].errors", hasSize(0))
                 .body("timeStampTokens[1].warning.content[0]", is("The time-stamp token does not cover container datafile!"))
-                .body("timeStampTokens[1].certificates", hasSize(3))
+                .body("timeStampTokens[1].certificates", hasSize(1))
     }
 
     def "Augmented XAdES signature validation report contains new archiveTimeStamps info"() {
